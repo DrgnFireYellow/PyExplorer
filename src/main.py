68a828813c6aaa6a36b1dd708e01b105a2ab5b37
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-03-02 11:35:47 -0500

    Format code with black

--- src/main.py
+++ src/main.py
@@ -20,21 +20,38 @@ pygame.display.set_caption("PyExplorer")
 solids = []
 
 player_right = pygame.image.load(os.path.join("textures", "player", "right.png"))
-player_right = pygame.transform.scale(player_right, (tiles.TILESIZE, tiles.TILESIZE * 2))
+player_right = pygame.transform.scale(
+    player_right, (tiles.TILESIZE, tiles.TILESIZE * 2)
+)
 
-player_left = pygame.transform.flip(player_right, True,  False)
+player_left = pygame.transform.flip(player_right, True, False)
 
 player_x = 0
 player_y = 0
 
+
 def load_map(map):
     global solids
     solids = []
-    for ycoord,  y in enumerate(map):
+    for ycoord, y in enumerate(map):
         for xcoord, x in enumerate(y):
-            window.blit(tiles.tiles[x], (xcoord * tiles.TILESIZE - player_x, ycoord * tiles.TILESIZE - player_y))
+            window.blit(
+                tiles.tiles[x],
+                (
+                    xcoord * tiles.TILESIZE - player_x,
+                    ycoord * tiles.TILESIZE - player_y,
+                ),
+            )
             if x:
-                solids.append(pygame.Rect(xcoord * tiles.TILESIZE - player_x, ycoord * tiles.TILESIZE - player_y - 2, tiles.TILESIZE, tiles.TILESIZE))
+                solids.append(
+                    pygame.Rect(
+                        xcoord * tiles.TILESIZE - player_x,
+                        ycoord * tiles.TILESIZE - player_y - 2,
+                        tiles.TILESIZE,
+                        tiles.TILESIZE,
+                    )
+                )
+
 
 direction = 1
 
@@ -74,47 +91,56 @@ while True:
             quit()
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_location = pygame.mouse.get_pos()
-            player_tile = (round(player_x / tiles.TILESIZE), round(player_y / tiles.TILESIZE))
-            mouse_tile = (math.floor(mouse_location[0] / tiles.TILESIZE) + player_tile[0], math.floor(mouse_location[1] / tiles.TILESIZE) + player_tile[1])
+            player_tile = (
+                round(player_x / tiles.TILESIZE),
+                round(player_y / tiles.TILESIZE),
+            )
+            mouse_tile = (
+                math.floor(mouse_location[0] / tiles.TILESIZE) + player_tile[0],
+                math.floor(mouse_location[1] / tiles.TILESIZE) + player_tile[1],
+            )
             if event.button == 3 and map[mouse_tile[1]][mouse_tile[0]] == 0:
                 map[mouse_tile[1]][mouse_tile[0]] = 1
             elif event.button == 1:
                 map[mouse_tile[1]][mouse_tile[0]] = 0
-    
+
     old_player_x = player_x
     old_player_y = player_y
     grounded = False
-
 
     keys = pygame.key.get_pressed()
 
     window.fill(pygame.Color("skyblue"))
     load_map(map)
     player_y += 4
-    
+
     if keys[pygame.K_RIGHT]:
         player_x += 2
         direction = 1
-    
+
     if keys[pygame.K_LEFT]:
         player_x -= 2
         direction = -1
 
-
-
-
-    player_rect = pygame.Rect(window.get_width() / 2, 0, tiles.TILESIZE, tiles.TILESIZE * 2)
+    player_rect = pygame.Rect(
+        window.get_width() / 2, 0, tiles.TILESIZE, tiles.TILESIZE * 2
+    )
 
     for solid in solids:
         if solid.colliderect(player_rect):
             if player_rect.midtop[1] < solid.midtop[1]:
                 grounded = True
                 player_y = old_player_y
-            if player_rect.midright[0] < solid.midright[0] and player_rect.midtop[1] + tiles.TILESIZE * 1.5 > solid.midtop[1]:
+            if (
+                player_rect.midright[0] < solid.midright[0]
+                and player_rect.midtop[1] + tiles.TILESIZE * 1.5 > solid.midtop[1]
+            ):
                 player_x -= 2
-            if player_rect.midright[0] > solid.midright[0] and player_rect.midtop[1] + tiles.TILESIZE * 1.5 > solid.midtop[1]:
+            if (
+                player_rect.midright[0] > solid.midright[0]
+                and player_rect.midtop[1] + tiles.TILESIZE * 1.5 > solid.midtop[1]
+            ):
                 player_x += 2
-    
 
     if keys[pygame.K_UP] and grounded:
         jump_remaining = tiles.TILESIZE * 2
@@ -123,12 +149,16 @@ while True:
         player_y -= 16
         jump_remaining -= 16
 
-
     if direction == 1:
         window.blit(player_right, (window.get_width() / 2, 0))
     elif direction == -1:
         window.blit(player_left, (window.get_width() / 2, 0))
-    window.blit(font.render(f"FPS: {clock.get_fps()}", False, pygame.Color("white")), (0, 0))
-    window.blit(font.render(f"Position: {player_x}, {player_y}", False, pygame.Color("white")), (0, 10))
+    window.blit(
+        font.render(f"FPS: {clock.get_fps()}", False, pygame.Color("white")), (0, 0)
+    )
+    window.blit(
+        font.render(f"Position: {player_x}, {player_y}", False, pygame.Color("white")),
+        (0, 10),
+    )
     pygame.display.update()
     clock.tick(60)
--- src/tiles.py
+++ src/tiles.py
@@ -12,8 +12,12 @@ tiles = {}
 for tile in os.listdir(TILESFOLDER):
     with open(os.path.join(TILESFOLDER, tile)) as tiledatafile:
         tiledata = load(tiledatafile, Loader)
-        tiles[tiledata["id"]] = pygame.image.load(os.path.join(TEXTUREFOLDER, tiledata["texture"]))
-        tiles[tiledata["id"]] = pygame.transform.scale(tiles[tiledata["id"]], (TILESIZE, TILESIZE))
+        tiles[tiledata["id"]] = pygame.image.load(
+            os.path.join(TEXTUREFOLDER, tiledata["texture"])
+        )
+        tiles[tiledata["id"]] = pygame.transform.scale(
+            tiles[tiledata["id"]], (TILESIZE, TILESIZE)
+        )
 
 tiles[0] = pygame.Surface((TILESIZE, TILESIZE))
 tiles[0].fill(pygame.Color("skyblue"))
